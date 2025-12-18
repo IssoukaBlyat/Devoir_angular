aad5d21cf58103b76d1f53c9725761985af66be7
Issouka <Titan.SSJ@gmx.com>
2025-12-18 17:28:45 +0100

    add final

--- src/app/sort-by-price.pipe.ts
+++ src/app/sort-by-price.pipe.ts
@@ -7,6 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SortByPricePipe implements PipeTransform {
 
   transform(produit: any[]): any [] {
+    console.log(produit);
     return produit.sort((a:any,b:any) => {
       if(a.prix < b.prix) { return -1;}
       else if(a.prix > b.prix) { return 1;}
